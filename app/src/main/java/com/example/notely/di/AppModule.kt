161be5b2dbd843885ce73723776cd1bcb76d8960
Gülten Doğan <63645518+gultendogan0@users.noreply.github.com>
2023-09-03 12:58:23 +0300

    added AddEditNoteViewModel

--- app/src/main/java/com/example/notely/di/AppModule.kt
+++ app/src/main/java/com/example/notely/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.example.notely.feature_note.data.repository.NoteRepositoryImpl
 import com.example.notely.feature_note.domain.repository.NoteRepository
 import com.example.notely.feature_note.domain.use_case.AddNote
 import com.example.notely.feature_note.domain.use_case.DeleteNote
+import com.example.notely.feature_note.domain.use_case.GetNote
 import com.example.notely.feature_note.domain.use_case.GetNotes
 import com.example.notely.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -41,7 +42,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
